a3fbc13b4ec574fbc0579f07081b64427b8a059f
AxelMartin69 <axel.tammekand@khk.ee>
2021-10-18 09:12:54 +0300

    OOP Complited

---
+++ classes/Task.js
@@ -0,0 +1,11 @@
+class Task {
+	constructor(name){
+		this.name = name;
+	}
+	addToUI(){
+		console.log(this.name + " is added to UÌ")
+	}
+	addedToLS(){
+		console.log(this.name + " is added to LS")
+	}
+}
